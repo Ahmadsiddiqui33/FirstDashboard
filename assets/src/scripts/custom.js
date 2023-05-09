47cf0e5be912a9accd582118517ae2048c728a99
Ahmad Siddiqui <mac@Macs-MacBook-Pro.local>
2023-05-09 19:32:10 +0500

    updated code

---
+++ assets/src/scripts/custom.js
@@ -0,0 +1,33 @@
+function display_modal() {
+    var modal = document.getElementById("table-modal");
+    if (modal.style.display === "block") {
+      modal.style.display = "none";
+    } else {
+      modal.style.display = "block";
+    }
+  }
+  
+  function modal_close() {
+    var modal = document.getElementById("table-modal");
+    if (modal.style.display === "block") {
+      modal.style.display = "none";
+    } else {
+      modal.style.display = "block";
+    }
+  }
+  function show_user() {
+    var modal = document.getElementById("profle_modal");
+    if (modal.style.display === "block") {
+      modal.style.display = "none";
+    } else {
+      modal.style.display = "block";
+    }
+  }
+  
+  function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
+  
